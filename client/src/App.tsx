57275f97b373571895a3fc14ae12f5af24828019
pididnothing <pranav24921@gmail.com>
2026-02-06 13:42:09 +0530

    build error-fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { User } from './types';
 import { getUser, getAllUsers } from './services/api';
